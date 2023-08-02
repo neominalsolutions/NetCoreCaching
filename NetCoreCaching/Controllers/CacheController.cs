0d9c3ce7c707c6689f034897c3a6664774d0128b
neominalsolutions <neominal.solutions@gmail.com>
2023-08-02 10:06:34 +0300

    Add project files.

---
+++ NetCoreCaching/Controllers/CacheController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreCaching.Services;
+
+namespace NetCoreCaching.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CacheController : ControllerBase
+  {
+    private readonly ICacheService _cacheService;
+
+    public CacheController(ICacheService cacheService)
+    {
+      _cacheService = cacheService;
+    }
+
+    [HttpGet("{key}/{value}")]
+    public async Task<IActionResult> Get(string key, string value)
+    {
+      // key value set ettik
+      // set ederken jsonString çalışmalıyız
+      //await _cacheService.Clear(key);
+      await _cacheService.SetValueAsync(key, value);
+      // value redisten keye göre çektik
+      var data = await _cacheService.GetValueAsync(key); // jsonString 
+
+      return Ok(data);
+    }
+
+  }
+}
---
+++ NetCoreCaching/Program.cs
@@ -0,0 +1,34 @@
+using NetCoreCaching.Services;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+IConfiguration configuration = builder.Configuration;
+var multiplexer = ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis"));
+builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer); // connection yöneten service
+
+builder.Services.AddScoped<ICacheService, RedisCacheService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
